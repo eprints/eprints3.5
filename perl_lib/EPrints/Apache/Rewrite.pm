1e822fc3ed383c4e22e98882538a615b50b2f11e
Don Cruickshank <dgc@soton.ac.uk>
2023-08-04 13:48:04 +0100

    Fixed issue with CGI scripts not having the correct load order. #34

--- perl_lib/EPrints/Apache/Rewrite.pm
+++ perl_lib/EPrints/Apache/Rewrite.pm
@@ -233,7 +233,10 @@ sub handler
 
 		##cgi path loaded earlier takes priority.
 
-		my @paths = reverse( EPrints::Init::get_lib_paths( $repository->{load_order}, 'cgi' ) );
+		# Get load order directly instead of pre-loaded version in $repository.
+		my $load_order = EPrints::Init::get_load_order( $repository->config( 'base_path' ), $repository->config( 'archiveroot' ) );
+
+		my @paths = reverse( EPrints::Init::get_lib_paths( $load_order, 'cgi' ) );
 		push @paths, EPrints::Config::get( "cgi_path" ); ##system cgi path
 
 		PATH: foreach my $path (@paths)
