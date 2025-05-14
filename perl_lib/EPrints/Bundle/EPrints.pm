d3282a590cc1d66fb514dd3835a23b9f34eaeb30
wr2f25 <wr2f25@soton.ac.uk>
2025-05-14 12:48:03 +0000

    Moved files that aren't installed by the bundle into a separate section at the bottom (so that they are still visible) and moved packages installed by perl 5.16+ into this section as well

--- perl_lib/EPrints/Bundle/EPrints.pm
+++ perl_lib/EPrints/Bundle/EPrints.pm
@@ -8,15 +8,9 @@ $VERSION = 'v3.5.0';
 
 BibTeX::Parser 1.05
 
-CGI 4.68 - 2.56 was included in perl v5.6.0
-
 Config::General 2.67
 
-DBD::mysql 5.012 - This actually still has to be installed by package manager as the config seems to break when installed by cpan
 DBI        1.647
-
-Digest::MD5 2.59 - 2.16 was included in perl v5.7.3
-Digest::SHA 6.04 - 5.32 was included in perl v5.9.3
 
 File::BOM   0.18
 File::Slurp 9999.32
@@ -24,13 +18,11 @@ File::Slurp 9999.32
 HTTP::Headers       7.00
 HTTP::Headers::Util 7.00
 
-Image::ExifTool 13.25 - Only used by pdf_metadata which is currently an ingredient
+Image::ExifTool 13.25
 
 IO::String 1.08
 
 JSON 4.10
-
-List::Util 1.68_01 - 1.06_00 was included in perl v5.7.3
 
 LWP::MediaTypes      6.04
 LWP::Protocol::https 6.14
@@ -40,9 +32,6 @@ LWP::UserAgent       6.78
 MIME::Lite 3.033
 
 Net::LDAP 0.68
-Net::SMTP 3.15 - 2.21 was included in perl v5.7.3
-
-Pod::Usage 2.05 - 1.12 was included in perl v5.6.0
 
 Search::Xapian 1.2.25.5
 
@@ -53,20 +42,40 @@ TeX::Encode 2.010
 Text::Extract::Word 0.04
 Text::Refer         1.106
 Text::Unidecode     1.30
-Text::Wrap          2024.001 - 98.112902 was included in perl v5.6.0
-
-Time::HiRes 1.9764 - 1.20_00 was included in perl v5.7.3
-Time::Seconds 1.36 - An unknown version was included in perl v5.9.5
-
-Unicode::Collate - 0.10 was included in perl v5.7.3
 
 URI          5.31
 URI::OpenURL 0.4.6
 
 XML::DOM              1.46
-XML::LibXML           2.0210     - XML::LibXSLT 2.003000 - This is imported manually in cpan_modules.pl because it has to be run with notest
+XML::LibXML           2.0210
 XML::NamespaceSupport 1.12
 XML::Parser           2.47
 
 YAML::Tiny            1.76
+
+=head1 OTHER PACKAGES
+
+=head2 These packages aren't installed by this file for various reasons but can be expected to be on the system
+
+CGI 3.59 - Included in Perl v5.16.1 (latest is 4.68)
+
+DBD::mysql 5.012 - This still has to be installed with a package manager for config reasons
+
+Digest::MD5 2.51 - Included in Perl v5.16.1 (latest is 2.59)
+Digest::SHA 5.71 - Included in Perl v5.16.1 (latest is 6.04)
+
+List::Util 1.25 - Included in Perl v5.16.1 (latest is 1.68_01)
+
+Net::SMTP 2.31 - Included in Perl v5.16.1 (latest is 3.15)
+
+Pod::Usage 1.51 - Included in Perl v5.16.1 (latest is 2.05)
+
+Text::Wrap 2009.0305 - Included in Perl v5.16.1 (latest is 2024.001)
+
+Time::HiRes 1.9725 - Included in Perl v5.16.1 (latest is 1.9764)
+Time::Seconds undef - Included in Perl v5.16.1 (latest is 1.36)
+
+Unicode::Collate 0.89 - Included in Perl v5.16.1 (latest is 1.31)
+
+XML::LibXSLT 2.003000 - This is installed separately in cpan_modules.pl because it has to be run with notest
 
