eprints/eprints3.5

commit ff687b85cfedf0b5b53b5826250c5cd29f60f462

Prevent a javascript error when there are no search results (#107)
* Prevent a javascript error when there are no search results

When there are no search results `thisElement` is `undefined` rather
than `null` because there are no 'ep_search_result' elements. So we can
instead do a truthy check which is fine because all valid values will be
`Element`s which are not falsy.

* Prevented empty/solely ignored word searches from making invalid regex

The regex construction code was sharing a bracket unnecessarily so that
if you made an empty search (invalid to actually search but what happens
if you just click the search button) or a search involving only ignored
words (like 'if and') it would end up with mismatched brackets in the
regex.

perl_lib/EPrints/Plugin/Screen/Search.pm
@@ -1045,7 +1045,7 @@ sub generate_regex
 	}
 
 	# Require a match to start with a word boundary (we can't use \b because it doesn't handle unicode well)
-	my $regex = '/(?<=^|[^\p{L}\p{N}])(?:';
+	my $regex = '/(?<=^|[^\p{L}\p{N}])';
 	my $last_word;
 	for my $word (@words) {
 		my $regex_word = $self->generate_regex_word( $word, $settings );
@@ -1059,7 +1059,8 @@ sub generate_regex
 			} else {
 				# Capture any spaces preceding a word, iff the last word is correctly the word before this,
 				# this allows it to join up words that follow each other in the search.
-				$regex .= "(?:(?:(?<=$last_word)[^\\p{L}\\p{N}]*)?" if $last_word;
+				$regex .= '(?:';
+				$regex .= "(?:(?<=$last_word)[^\\p{L}\\p{N}]*)?" if $last_word;
 				$regex .= $regex_word . '(?=$|[^\p{L}\p{N}]))?';
 				$last_word = $regex_word;
 			}

lib/static/javascript/search_highlighter.js
@@ -114,7 +114,7 @@ function highlightSearch(highlight) {
 		} else {
 			var thisElement = previousElement.nextElementSibling;
 		}
-		if (thisElement === null) return;
+		if (!thisElement) return;
 
 		highlight(thisElement);
 		previousElement = thisElement;

commit fc4f25452901689d342cd3cc03e9d0904eb95aa2

Fix a regex issue with highlighting facets caused by #107

perl_lib/EPrints/Plugin/Screen/Search.pm
@@ -1076,7 +1076,7 @@ sub generate_regex
 		$regex .= '(?<=[\p{L}\p{N}])';
 	} else {
 		# Require a match to end on a (unicode) word boundary
-		$regex .= ')(?=$|[^\p{L}\p{N}])';
+		$regex .= '(?=$|[^\p{L}\p{N}])';
 	}
 
 	return $regex . '/gmiu';