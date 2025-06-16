be0983c920e23cdf2925ce10ce1e6a0ca004f7ae
David R Newman <drn@ecs.soton.ac.uk>
2025-06-16 20:36:41 +0000

    Updates version and related config.
    In copyright/licence boilerplate for config files defining version.
    Adds extra maintainers to core and pub_lib component.
    Switches pub and zero flavours to use same version number as core.
    Removes eprints_classic_styles ingredient to use new bootstap layout.

--- lib/defaultcfg_zero/cfg.d/flavour_info.pl
+++ lib/defaultcfg_zero/cfg.d/flavour_info.pl
@@ -1,35 +1,35 @@
 $c->{flavour_id} = "zero";
 $c->{flavour_name} = "Zero";
-$c->{flavour_version} = "1.3";
+$c->{flavour_version} = "3.5.0";
 
 
 =head1 COPYRIGHT
 
 =for COPYRIGHT BEGIN
 
-Copyright 2022 University of Southampton.
-EPrints 3.4 is supplied by EPrints Services.
+Copyright 2025 University of Southampton.
+EPrints 3.5.0 Beta 1 is supplied by EPrints Services.
 
-http://www.eprints.org/eprints-3.4/
+https://github.com/eprints3/eprints3.5
 
 =for COPYRIGHT END
 
 =for LICENSE BEGIN
 
-This file is part of EPrints 3.4 L<http://www.eprints.org/>.
+This file is part of EPrints 3.5.0 Beta 1 L<http://www.eprints.org/>.
 
-EPrints 3.4 and this file are released under the terms of the
+EPrints 3.5.0 Beta 1 and this file are released under the terms of the
 GNU Lesser General Public License version 3 as published by
 the Free Software Foundation unless otherwise stated.
 
-EPrints 3.4 is distributed in the hope that it will be useful,
+EPrints 3.5.0 Beta 1 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.4.
-If not, see L<http://www.gnu.org/licenses/>.
+License along with EPrints 3.5.0 Beta 1.
+If not, see L<https://www.gnu.org/licenses/>.
 
 =for LICENSE END
 
