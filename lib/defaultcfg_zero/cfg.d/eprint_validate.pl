849979955f8519a95652b4111d6f6ca0c4983707
wr2f25 <wr2f25@soton.ac.uk>
2025-10-07 14:41:47 +0000

    Re-add unused trigger files as demonstrations
    While neither `user_validate` or `eprint_validate` actually did anything
    they were a useful reference point to explain how to validate these
    objects so this re-adds them (previously removed in c112ed2) and just
    edits them to point towards `EP_TRIGGER_VALIDATE` rather than
    `*_validate`.

---
+++ lib/defaultcfg_zero/cfg.d/eprint_validate.pl
@@ -0,0 +1,60 @@
+######################################################################
+#
+# EP_TRIGGER_VALIDATE 'eprint' dataset trigger
+#
+######################################################################
+#
+# $dataobj 
+# - EPrint object
+# $repository 
+# - Repository object (the current repository)
+# $for_archive
+# - Is this being checked to go live (`1` means it is)
+# $problems
+# - ARRAYREF of DOM objects
+#
+######################################################################
+#
+# Validate the whole eprint, this is the last part of a full 
+# validation so you don't need to duplicate tests in 
+# validate_eprint_meta, validate_field or validate_document.
+#
+######################################################################
+
+#$c->add_dataset_trigger( 'user', EPrints::Const::EP_TRIGGER_VALIDATE, sub {
+#	my( %args ) = @_;
+#	my( $repository, $eprint, $for_archive, $problems ) = @args{qw( repository dataobj for_archive problems )};
+#
+#	push @$problems, $repository->make_text( 'Demo eprint validation trigger' );
+#}, id => 'demo_id' );
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2022 University of Southampton.
+EPrints 3.4 is supplied by EPrints Services.
+
+http://www.eprints.org/eprints-3.4/
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints 3.4 L<http://www.eprints.org/>.
+
+EPrints 3.4 and this file are released under the terms of the
+GNU Lesser General Public License version 3 as published by
+the Free Software Foundation unless otherwise stated.
+
+EPrints 3.4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints 3.4.
+If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+
