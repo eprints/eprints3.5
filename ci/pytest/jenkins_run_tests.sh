8c8fae7f5b373cf51a493981706da26a6a72a1c8
Luke Wallin <lpw1r25@soton.ac.uk>
2026-03-26 10:38:02 +0000

    trying from different dir

--- ci/pytest/jenkins_run_tests.sh
+++ ci/pytest/jenkins_run_tests.sh
@@ -8,5 +8,10 @@ source /home/eprints/playwright-venv/bin/activate
 
 printenv
 
+cd `dirname "$(realpath $0)"`
+
+echo `pwd`
+
 #--rootdir `pwd` 
+
 python -m pytest --creds $CREDS_FILE --url https://playwright.eprints-hosting.org/
