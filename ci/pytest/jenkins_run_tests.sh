9cae968749b33e803fa4a145ce9e79cf89d5cc00
Luke Wallin <lpw1r25@soton.ac.uk>
2026-03-26 12:43:00 +0000

    tidying up commented out code

--- ci/pytest/jenkins_run_tests.sh
+++ ci/pytest/jenkins_run_tests.sh
@@ -2,12 +2,12 @@
 
 CREDS_FILE=$1
 
-echo `pwd`
-
-
-
+#to create again:
+# python -m venv playwright-venv
+# source playwright-venv/bin/activate
+# pip install pytest fpdf2 playwright pytest-playwright
+# playwright install
 source /home/eprints/playwright-venv/bin/activate
-
 
 
 thisfilepath=`dirname "$(realpath $0)"`
@@ -16,19 +16,8 @@ echo `pwd`
 export WORKSPACE=`pwd`
 
 
-# python -m venv jenkins-venv
-# source jenkins-venv/bin/activate
-
-# pip install pytest fpdf2 playwright pytest-playwright
-# playwright install
-
-
-printenv
-
-echo `which pytest`
 #supplying CREDS_FILE on command line to pytest results in jenkins doing very weird things to the environment and pytest being unable to find its tests.
 cp $CREDS_FILE credentials.json
-
 
 pytest --url https://testnode-1.eprints-hosting.org/  --junitxml=results.xml
 
