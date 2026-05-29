90a87edf33feebbd4ac1c4a443f37e847d5ec618
David R Newman <drn@ecs.soton.ac.uk>
2026-05-29 15:22:54 +0100

    Updates licensing and copyright info (#380)
    * Updates licensing and copyright info
    
    - Removes duplicate licensing info under license/
    - Move LGPL text in COPYING to LICENSE
    - Updates README to add latest copyright year and reference LGPL rather the GPL.
    - Updates licensing/copyright boilerplate in each file to be more terse and point at README
    
    * Pulls through latest version of pub_lib flavour and various ingredients

--- perl_lib/EPrints/Init.pm
+++ perl_lib/EPrints/Init.pm
@@ -298,33 +298,12 @@ sub update_inc_paths
 
 ######################################################################
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-=begin COPYRIGHT
+=begin COPYRIGHT_AND_LICENSE
 
-Copyright 2023 University of Southampton.
+Copyright University of Southampton under the GNU Lesser General Public License. See README at https://github.com/eprints/eprints3.5 for further information.
+
 EPrints 3.5 is supplied by EPrints Services.
 
-https://www.eprints.org/eprints-3.5/
-
-=end COPYRIGHT
-
-=begin LICENSE
-
-This file is part of EPrints 3.5 L<https://www.eprints.org/>.
-
-EPrints 3.5 and this file are released under the terms of the
-GNU Lesser General Public License version 3 as published by
-the Free Software Foundation unless otherwise stated.
-
-EPrints 3.5 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.5.
-If not, see L<https://www.gnu.org/licenses/>.
-
-=end LICENSE
-
+=end COPYRIGHT_AND_LICENSE
